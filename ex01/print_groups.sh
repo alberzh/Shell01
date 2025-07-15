11be66ee0e903d5a328f19aef257a2ed9491fa9e
Alberto Zaragoza <alberzh@gmail.com>
2025-07-15 18:39:00 +0200

    Añadido ex04 y corrección de ex01.

--- ex01/print_groups.sh
+++ ex01/print_groups.sh
@@ -6,8 +6,12 @@
 #    By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/14 16:23:18 by azaragoz          #+#    #+#              #
-#    Updated: 2025/07/15 16:24:08 by azaragoz         ###   ########.fr        #
+#    Updated: 2025/07/15 18:29:47 by azaragoz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-groups $FT_USER
+# -G muestra los identificadores numéricos de los grupos a los que pertenece el usuario
+# -n muestra los nombres de los grupos en lugar de sus identificadores numéricos
+#  Primer tr convierte los espacios en comas
+
+id -Gn "$FT_USER" | tr ' ' ','
--- ex04/MAC.sh
+++ ex04/MAC.sh
@@ -1,13 +1,16 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    count_files.sh                                     :+:      :+:    :+:    #
+#    MAC.sh                                             :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/07/15 16:22:45 by azaragoz          #+#    #+#              #
-#    Updated: 2025/07/15 16:28:06 by azaragoz         ###   ########.fr        #
+#    Created: 2025/07/15 16:45:14 by azaragoz          #+#    #+#              #
+#    Updated: 2025/07/15 18:38:24 by azaragoz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-find . | wc -l 
+# El filtrado de awk busca líneas que contengan "ether"
+# Y luego imprime el segundo campo, en este casola dirección MAC.
+
+ifconfig | awk '/ether/{print $2}'
