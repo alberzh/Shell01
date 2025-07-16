2af0bebddc888f2d43857169a339fb2aa4d9c61a
Alberto Zaragoza <alberzh@gmail.com>
2025-07-16 18:55:33 +0200

    Nueva corrección de ex02.

--- ex02/find_sh.sh
+++ ex02/find_sh.sh
@@ -6,7 +6,7 @@
 #    By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/14 16:23:10 by azaragoz          #+#    #+#              #
-#    Updated: 2025/07/16 17:39:50 by azaragoz         ###   ########.fr        #
+#    Updated: 2025/07/16 18:53:18 by azaragoz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,4 +16,4 @@
 # $NF = "" elimina la última columna (extensión .sh en nuestro caso) asignando un valor vacío
 # print $0 imprime la línea completa después de haber eliminado la extensión
 
-find . -name "*.sh" | awk -F "." '{ $NF = "" ; print $0 }' | awk -F "/" '{print $NF}
+find . -name "*.sh" | awk -F "." '{ $NF = "" ; print $0 }'
