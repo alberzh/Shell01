959a954dedce7b68a2b4dd5f926bd18095b121f5
Alberto Zaragoza <alberzh@gmail.com>
2025-07-16 15:52:23 +0200

    Corrección ex01.

--- ex01/print_groups.sh
+++ ex01/print_groups.sh
@@ -6,7 +6,7 @@
 #    By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/14 16:23:18 by azaragoz          #+#    #+#              #
-#    Updated: 2025/07/15 18:29:47 by azaragoz         ###   ########.fr        #
+#    Updated: 2025/07/16 15:50:24 by azaragoz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,4 +14,4 @@
 # -n muestra los nombres de los grupos en lugar de sus identificadores numéricos
 #  Primer tr convierte los espacios en comas
 
-id -Gn "$FT_USER" | tr ' ' ','
+id -Gn "$FT_USER" | sed 's/ /,/g'
