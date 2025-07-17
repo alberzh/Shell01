6a739d80da3cd3f5131a8cd8b0e52904aa2f814e
Alberto Zaragoza <alberzh@gmail.com>
2025-07-17 12:09:53 +0200

    Ejercicio ex01 corregido.

--- ex01/print_groups.sh
+++ ex01/print_groups.sh
@@ -6,11 +6,12 @@
 #    By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/14 16:23:18 by azaragoz          #+#    #+#              #
-#    Updated: 2025/07/16 17:35:06 by azaragoz         ###   ########.fr        #
+#    Updated: 2025/07/17 12:09:30 by azaragoz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # -G muestra los identificadores numéricos de los grupos a los que pertenece el usuario
 # -n muestra los nombres de los grupos en lugar de sus identificadores numéricos
+# Con el tr sustituimos lo que le digamos, en este caso con -d "borramos" los saltos de línea "\n"
 
-id -Gn "$FT_USER" | sed 's/ /,/g'
+id -Gn "$FT_USER" | sed 's/ /,/g' | tr -d '\n'
--- ex02/find_sh.sh
+++ ex02/find_sh.sh
@@ -1,19 +1,1 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    find_sh.sh                                         :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/07/14 16:23:10 by azaragoz          #+#    #+#              #
-#    Updated: 2025/07/16 18:53:18 by azaragoz         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-# find . te busca en el directorio actual y subdirectorios 
-# -name "*.sh" busca archivos con extensión .sh
-# awk -F "." divide la línea por el punto
-# $NF = "" elimina la última columna (extensión .sh en nuestro caso) asignando un valor vacío
-# print $0 imprime la línea completa después de haber eliminado la extensión
-
 find . -name "*.sh" | awk -F "." '{ $NF = "" ; print $0 }'
